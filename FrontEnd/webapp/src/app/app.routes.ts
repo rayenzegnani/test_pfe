756e8853aca618c923d59b3d5cbf3d5b4b358525
omarhabib5 <oh8852237@gmail.com>
2025-10-17 16:12:43 +0100

    all

--- FrontEnd/webapp/src/app/app.routes.ts
+++ FrontEnd/webapp/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import {Categories} from './components/manage/categories/categories';
+import {CategoryForm} from './components/manage/category-form/category-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'',component:Home},
+    {path:'admin/category',component:Categories},
+      {path:'admin/category/add',component:CategoryForm},
+         {path:'admin/category/:id',component:CategoryForm},
+
+];
---
+++ FrontEnd/webapp/src/app/components/manage/categories/categories.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  imports:[ MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  templateUrl: './categories.html',
+  styleUrl: './categories.scss'
+})
+export class Categories {
+
+  displayedColumns: string[] = ['id', 'Name', 'action'];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+}
